69fc2bfcca91471b7fa0191451681d427c35def7
Vinícius Serra <vssm@cin.ufpe.br>
2018-12-03 12:31:58 -0200

    Update img_process.py

--- img_process.py
+++ img_process.py
@@ -1,62 +1,33 @@
+import cv2 as cv
 import numpy as np
-import math
-from scipy import ndimage
-from scipy import misc
-import cv2
 
+def process(frame):
+	if frame is None:
+		return -1
 
-def getDist(v1, v2):
-	ans = 0
-	for i in range(3):
-		ans += abs(v1[i] - v2[i])
+	cv.imwrite('before.png', frame) #just saving the image for debugging purposes
+	frame_HSV = cv.cvtColor(frame, cv.COLOR_BGR2HSV) #transforming to HSV format
+	ft = cv.inRange(frame_HSV, (0, 0, 0), (180, 255, 10)) #using treshold in image, only pixels in range will appear white
 
-	return ans
-
-def isRed(v1):
-	return getDist(v1, [133, 55, 50]) < 100
-
-def process():
-	img = misc.imread('test.png')
-	n, m = img.shape[0], img.shape[1]
-
-	ans = 0
-	vis = n*[m*[0]]
-
-	def valid(x, y):
-		return x >= 0 and x < n and y >= 0 and y < m and isRed(img[x][y]) and vis[x][y] == 0
-
-	def dfs(x, y):
-		vis[x][y] = 1 
-		tam = 1
-		for i in range(-10, 10):
-			for j in range(-10, 10):
-				nx, ny = x + i, y + j
-				if valid(nx, ny):
-					tam += dfs(nx, ny)
-
-		return tam
-
-	for i in range(n):
-		for j in range(m):
-			if isRed(img[i][j]):
-				if vis[i][j] == 0:
-					sz = dfs(i, j)
-					if sz >= 15:
-						ans += 1
-
-	result = open('number_dots.bin', 'wb')
-	ret = bytearray([ans])
-	result.write(ret)
-	result.close()
+	kernel = np.ones((5,5),np.uint8) #kernel of transformation
+	ft = cv.morphologyEx(ft, cv.MORPH_OPEN, kernel) #opennig image, removing background noise
+	ft = cv.morphologyEx(ft, cv.MORPH_CLOSE, kernel) #closing image, filling small holes
+	
+	cv.imwrite('after.png', ft) #just saving transformed image for debugging purposes
+	im2, contours, hierarchy = cv.findContours(ft, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE) #counting number of countours
+	return len(contours)
 
 def captureImg():
-	cap = cv2.VideoCapture(0+cv2.CAP_DSHOW)
-	if cap.isOpened() == False:
-		cap.open()
+	cap = cv.VideoCapture(0)
+
+	if cap is None:
+		return None
 
 	ret, frame = cap.read()
-	if ret == True:
-		cv2.imwrite('test.png', frame)
+	if ret is False:
+		return None
+
+	return frame
 
 if __name__ == '__main__':
 	while True:
@@ -64,6 +35,9 @@ if __name__ == '__main__':
 		value = np.fromfile(checker, dtype = np.uint32)
 		checker.close()
 		if value == 1:
-			captureImg()
-			process()
-
+			img = captureImg()	
+			nObj = process(img)
+			retFile = open('number_dots.bin', 'wb')
+			ret = bytearray([nObj])
+			retFile.write(ret)
+			retFile.close()
